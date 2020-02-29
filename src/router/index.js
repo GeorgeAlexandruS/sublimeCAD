591ad917a31161c4eb67fb9ff62d40b5490f78ad
eorgeAlexandruS <g.sfica@gmail.com>
2020-02-29 00:11:50 +0000

    router updated

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Router from 'vue-router'
 import Home from '@/views/Home.vue'
 import Login from '@/views/authentication/Login.vue'
 import Register from '@/views/authentication/Register.vue'
@@ -7,47 +7,44 @@ import TasksAll from '@/views/tasks/TasksAll.vue'
 import TasksCreate from '@/views/tasks/TasksCreate.vue'
 import TasksEdit from '@/views/tasks/TasksEdit.vue'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: './views/tasks',
-    name: 'tasks-all',
-    component: TasksAll
-  },
-  {
-    path: '/tasks/new',
-    name: 'tasks-create',
-    component: TasksCreate
-  },
-  {
-    path: '/tasks:id',
-    name: 'tasks-edit',
-    component: TasksEdit
-  },
-  {
-    path: '/register',
-    name: 'register',
-    component: Register
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: Login
-  },
-  {
-    path: '*',
-    redirect: '/'
-  }
-]
-
-const router = new VueRouter({
-  routes
+export default new Router({
+  routes: [{
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: './views/tasks',
+      name: 'tasks-all',
+      component: TasksAll
+    },
+    {
+      path: '/tasks/new',
+      name: 'tasks-create',
+      component: TasksCreate
+    },
+    {
+      path: '/tasks:id',
+      name: 'tasks-edit',
+      component: TasksEdit
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '*',
+      redirect: '/'
+    }
+  ],
+  linkActiveClass: 'active',
+  mode: 'history'
 })
-
-export default router
