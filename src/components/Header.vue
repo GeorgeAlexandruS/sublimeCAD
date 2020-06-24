012e2175d7a09f55d2786755f7ad39dcc0ebab1d
George_Alexandru <g.sfica@gmail.com>
2020-06-24 08:36:46 +0100

    frontend updates

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,52 +1,53 @@
 <template>
-  <div class="">
+<div class="">
 
 
-    <!--====================================================================
+  <!--====================================================================
         Start hero section
 =====================================================================-->
 
-    <section class="hero-section con-cloud-hero bg-img d-flex align-items-center rpb-120 custom-background">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-7">
-            <div class="con-cloud-hero-content">
-              <h1>Most of the efficiency <br>in design is lost <br>in repetitive tasks</h1>
-              <p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, <br>magna a ullamcorper laoreet, lectus arcu pulvinar risus.</p>
-              <form action="#">
-                <input type="email" placeholder="Enter Your Email" required>
-                <button class="con-cloud-btn" type="submit"><span>Subscribe</span></button>
-              </form>
-            </div>
+  <section class="hero-section con-cloud-hero bg-img d-flex align-items-center rpb-120 custom-background">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-7">
+          <div class="con-cloud-hero-content">
+            <h1>SublimeCAD</h1>
+            <h2 style="color:grey;text-align:left;">Design efficiency</h2>
+            <p>AutoCAD plugins | Automation | 3D Configurators</p>
+            <form action="#">
+              <input type="email" placeholder="Enter Your Email" required>
+              <button class="con-cloud-btn" type="submit"><span>Subscribe</span></button>
+            </form>
           </div>
-
-          <div class="col-lg-5">
-            <div class="cc-hero-images rmt-50">
-              <div class="cc-server wow customFadeInRight slow delay-1s">
-
-                <img src="../assets/img/con-cloud/cc-hero-5.png" alt="Hero Image">
-              </div>
-              <div class="cc-server-pc wow customFadeInDown slow">
-                <img src="../assets/img/con-cloud/cc-hero-4.png" alt="Hero Image">
-              </div>
-              <div class="cc-tile-one">
-                <img src="../assets/img/con-cloud/cc-hero-3.png" alt="Hero Image">
-              </div>
-              <div class="cc-tile-two">
-                <img src="../assets/img/con-cloud/cc-hero-2.png" alt="Hero Image">
-              </div>
-              <div class="cc-tile-three">
-                <img src="../assets/img/con-cloud/cc-hero-1.png" alt="Hero Image">
-              </div>
-            </div>
-          </div>
-
         </div>
+
+        <div class="col-lg-5">
+          <div class="cc-hero-images rmt-50">
+            <div class="cc-server wow customFadeInRight slow delay-1s">
+
+              <img src="../assets/img/con-cloud/cc-hero-5.png" alt="Hero Image">
+            </div>
+            <div class="cc-server-pc wow customFadeInDown slow">
+              <img src="../assets/img/con-cloud/cc-hero-4.png" alt="Hero Image">
+            </div>
+            <div class="cc-tile-one">
+              <img src="../assets/img/con-cloud/cc-hero-3.png" alt="Hero Image">
+            </div>
+            <div class="cc-tile-two">
+              <img src="../assets/img/con-cloud/cc-hero-2.png" alt="Hero Image">
+            </div>
+            <div class="cc-tile-three">
+              <img src="../assets/img/con-cloud/cc-hero-1.png" alt="Hero Image">
+            </div>
+          </div>
+        </div>
+
       </div>
-    </section>
-    <!--====================================================================
+    </div>
+  </section>
+  <!--====================================================================
         End hero section
 =====================================================================-->
 
-  </div>
+</div>
 </template>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,98 +1,110 @@
+
+
 <template>
 
-  <header class="main-header">
+<header class="main-header">
 
     <!--Header-Upper-->
     <div class="header-upper">
-      <div class="container clearfix">
+        <div class="container clearfix">
 
-        <div class="header-inner clearfix d-lg-flex">
-          <div class="logo-outer">
-            <router-link to="/" class="navbar-brand">
-              <div class="logo"><a href="index.html"><img src="../assets/img/logo/sublime-logo.png" alt="" title=""></a></div>
-              <div class="fixed-logo"><a href="index.html"><img src="../assets/img/logo/sublime-logo.png" alt="" title=""></a></div>
-            </router-link>
-          </div>
+            <div class="header-inner clearfix d-lg-flex">
+                <div class="logo-outer">
+                    <router-link to="/" class="navbar-brand">
+                        <div class="logo">
+                            <a href="index.html"><img src="../assets/img/logo/sublime-logo.png" alt="" title=""></a>
+                        </div>
+                        <div class="fixed-logo">
+                            <a href="index.html"><img src="../assets/img/logo/sublime-logo.png" alt="" title=""></a>
+                        </div>
+                    </router-link>
+                </div>
 
-          <!-- Main Menu -->
-          <nav class="main-menu navbar-expand-md ml-md-auto">
-            <div class="navbar-header clearfix">
-              <div class="logo"><a href="index.html"><img src="../assets/img/logo/sublime-logo.png" alt="" title=""></a></div>
-              <div class="fixed-logo"><a href="index.html"><img src="../assets/img/logo/sublime-logo.png" alt="" title=""></a></div>
-              <!-- Toggle Button -->
-              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse-one">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-            </div>
+                <!-- Main Menu -->
+                <nav class="main-menu navbar-expand-md ml-md-auto">
+                    <div class="navbar-header clearfix">
+                        <div class="logo">
+                            <a href="index.html"><img src="../assets/img/logo/sublime-logo.png" alt="" title=""></a>
+                        </div>
+                        <div class="fixed-logo">
+                            <a href="index.html"><img src="../assets/img/logo/sublime-logo.png" alt="" title=""></a>
+                        </div>
+                        <!-- Toggle Button -->
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse-one">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    </div>
 
-            <div class="navbar-collapse navbar-collapse-one collapse clearfix">
-              <ul class="navigation clearfix">
+                    <div class="navbar-collapse navbar-collapse-one collapse clearfix">
+                        <ul class="navigation clearfix">
 
-                <li class="current dropdown">
-                  <router-link to="/" class="nav-link" exact>
-                    Home
-                  </router-link>
-                </li>
+                            <li class="current dropdown">
+                                <router-link to="/" class="nav-link" exact>
+                                    Home
+                                </router-link>
+                            </li>
 
-                <li class="dropdown">
-                  <router-link to="/blogs" class="nav-link" exact>
-                    Blog
-                  </router-link>
-                </li>
+                            <li class="dropdown">
+                                <router-link to="/blogs" class="nav-link" exact>
+                                    Blog
+                                </router-link>
+                            </li>
 
-                <li class="dropdown"><a href="#">Tools</a></li>
-                <li class="dropdown"><a href="#">Examples</a></li>
+                            <li class="dropdown"><a href="#">Tools</a></li>
+                            <li class="dropdown"><a href="#">Examples</a></li>
 
-                <li v-if="!$store.state.isLoggedIn" class="login">
-                  <router-link to="/login" class="nav-link" exact>
-                    LOGIN
-                  </router-link>
-                </li>
+                            <li v-if="!$store.state.isLoggedIn" class="login">
+                                <router-link to="/login" class="nav-link" exact>
+                                    LOGIN
+                                </router-link>
+                            </li>
 
-                <li v-if="!$store.state.isLoggedIn" class="register">
-                  <router-link to="/register" class="nav-link" exact>
-                    REGISTER
-                  </router-link>
-                </li>
+                            <li v-if="!$store.state.isLoggedIn" class="register">
+                                <router-link to="/register" class="nav-link" exact>
+                                    REGISTER
+                                </router-link>
+                            </li>
 
-                <li v-if="$store.state.isLoggedIn" class="logout">
-                  <a v-on:click.prevent="logout()" class="nav-link" href="#">Logout</a>
-                </li>
+                            <li v-if="$store.state.isLoggedIn" class="logout">
+                                <a v-on:click.prevent="logout()" class="nav-link" href="#">Logout</a>
+                            </li>
 
-                <li v-if="$store.state.isLoggedIn" class="Disabled">
-                  <!-- Display the current user's username in the navbar -->
-                  <a class="nav-link" href="#">
+                            <li v-if="$store.state.isLoggedIn" class="Disabled">
+                                <!-- Display the current user's username in the navbar -->
+                                <a class="nav-link" href="#">
                     Hi, {{ this.$store.state.username ? this.$store.state.username : 'User' }}
                   </a>
-                </li>
+                            </li>
 
-              </ul>
+                        </ul>
 
+                    </div>
+                </nav>
             </div>
-          </nav>
         </div>
-      </div>
     </div>
     <!--End Header Upper-->
 
-  </header>
+</header>
 
 </template>
 
 <script>
-  import * as auth from '../services/AuthService';
 
-  export default {
+import * as auth from '../services/AuthService';
+
+export default {
     name: 'Navbar',
     methods: {
-      logout: function() {
-        auth.logout();
-        this.$router.push({
-          name: 'home'
-        });
-      }
+        logout: function() {
+            auth.logout();
+            this.$router.push({
+                name: 'home'
+            });
+        }
     }
-  }
+}
+
 </script>
